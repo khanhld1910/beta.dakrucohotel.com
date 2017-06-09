48ea0919e9548ed8d347696400630b2ea0dc8395
Khanh Luong <khanhld1910@gmail.com>
2017-06-09 16:37:47 +0700

    updated

---
+++ Content/myScripts/myScripts.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('.slider').slider({
+        indicators: false,
+        height: 400,
+        transition: 500,
+        interval: 5000
+    });
+    $(".button-collapse").sideNav();
+
+    // set columns' height are all the same in grid
+    $(".table-row").each(function () {
+        var maxHeight = 0;
+        $(this).children().each(function () {
+            maxHeight = maxHeight > $(this).height()
+                ? maxHeight : $(this).height();
+        });
+        $(this).children().height(maxHeight);
+        //console.log(heights);
+    });
+    // My Coding-Styles Romance by L.D.K aka "Dolphin Dreams"
+});
